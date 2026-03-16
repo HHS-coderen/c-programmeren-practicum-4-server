d16547413794400a3c6c86b70536c0f627696e71
Max de Jong <maxdejong06@gmail.com>
2026-03-16 19:12:15 +0100

    1a en 1b af

--- c-programmeren-practicum-4-server/practicum4.1-server.c
+++ c-programmeren-practicum-4-server/practicum4.1-server.c
@@ -1,47 +1,61 @@
 #include <stdio.h>
+#include <string.h>
 #include <winsock2.h>
+#include <ws2tcpip.h>
 #pragma comment(lib, "Ws2_32.lib")
 #pragma warning(disable:4996)
 
 #define DEFAULT_PORT "27015"
+#define DEFAULT_BUFLEN 512
 
 int main() {
-	WSADATA wsaData;
-	int iResult;
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-	// Initialize Winsock
-	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-	if (iResult != 0) {
-		printf("WSAStartup failed: %d\n", iResult);
-		return 1;
-	}
+    struct addrinfo* result = NULL, hints;
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = AI_PASSIVE;
 
-	struct addrinfo* result = NULL, * ptr = NULL, hints;
+    getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
 
-	ZeroMemory(&hints, sizeof(hints));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_protocol = IPPROTO_TCP;
-	hints.ai_flags = AI_PASSIVE;
+    SOCKET ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
 
-	// Resolve the local address and port to be used by the server
-	iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
-	if (iResult != 0) {
-		printf("getaddrinfo failed: %d\n", iResult);
-		WSACleanup();
-		return 1;
-	}
-	SOCKET ListenSocket = INVALID_SOCKET;
+    if (bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen) == SOCKET_ERROR) {
+        printf("bind() mislukt: %d\n", WSAGetLastError());
+        return 1;
+    }
+    freeaddrinfo(result);
 
-	// Create a SOCKET for the server to listen for client connections
+    if (listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR) {
+        printf("listen() mislukt: %d\n", WSAGetLastError());
+        return 1;
+    }
+    printf("Server luistert op poort %s...\n", DEFAULT_PORT);
 
-	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    SOCKET ClientSocket = accept(ListenSocket, NULL, NULL);
+    if (ClientSocket == INVALID_SOCKET) {
+        printf("accept() mislukt: %d\n", WSAGetLastError());
+        return 1;
+    }
+    printf("Client verbonden!\n");
+    closesocket(ListenSocket);
 
-	if (ListenSocket == INVALID_SOCKET) {
-		printf("Error at socket(): %ld\n", WSAGetLastError());
-		freeaddrinfo(result);
-		WSACleanup();
-		return 1;
-	}
+    char recvbuf[DEFAULT_BUFLEN];
+    int iResult;
+    do {
+        iResult = recv(ClientSocket, recvbuf, DEFAULT_BUFLEN - 1, 0);
+        if (iResult > 0) {
+            recvbuf[iResult] = '\0';
+            printf("Ontvangen: %s\n", recvbuf);
+            const char* antwoord = "Hallo van de server!";
+            send(ClientSocket, antwoord, (int)strlen(antwoord), 0);
+        }
+    } while (iResult > 0);
 
+    closesocket(ClientSocket);
+    WSACleanup();
+    return 0;
 }
