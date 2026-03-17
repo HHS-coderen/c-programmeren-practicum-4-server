82001bfa7262737b06fc281a94c096b340e62bcd
Max de Jong <167071016+Maxhmmm@users.noreply.github.com>
2026-03-17 19:25:50 +0100

    maken opdracht 2

--- c-programmeren-practicum-4-server/practicum4.1-server.c
+++ c-programmeren-practicum-4-server/practicum4.1-server.c
@@ -8,6 +8,13 @@
 #define DEFAULT_PORT "27015"
 #define DEFAULT_BUFLEN 512
 
+const char* zoekWaarde(const char* key) {
+    if (strcmp(key, "temperatuur") == 0)        return "22.5";
+    if (strcmp(key, "luchtvochtigheid") == 0)   return "65";
+    if (strcmp(key, "ventilatorsnelheid") == 0) return "1200";
+    return "onbekend";
+}
+
 int main() {
     WSADATA wsaData;
     WSAStartup(MAKEWORD(2, 2), &wsaData);
@@ -22,40 +29,40 @@ int main() {
     getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
 
     SOCKET ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-
-    if (bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen) == SOCKET_ERROR) {
-        printf("bind() mislukt: %d\n", WSAGetLastError());
-        return 1;
-    }
+    bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
     freeaddrinfo(result);
-
-    if (listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR) {
-        printf("listen() mislukt: %d\n", WSAGetLastError());
-        return 1;
-    }
+    listen(ListenSocket, SOMAXCONN);
     printf("Server luistert op poort %s...\n", DEFAULT_PORT);
 
-    SOCKET ClientSocket = accept(ListenSocket, NULL, NULL);
-    if (ClientSocket == INVALID_SOCKET) {
-        printf("accept() mislukt: %d\n", WSAGetLastError());
-        return 1;
+    while (1) {
+        SOCKET ClientSocket = accept(ListenSocket, NULL, NULL);
+        if (ClientSocket == INVALID_SOCKET) break;
+        printf("Client verbonden!\n");
+
+        char recvbuf[DEFAULT_BUFLEN];
+        int iResult;
+        do {
+            iResult = recv(ClientSocket, recvbuf, DEFAULT_BUFLEN - 1, 0);
+            if (iResult > 0) {
+                recvbuf[iResult] = '\0';
+
+                char key[DEFAULT_BUFLEN] = "";
+                char sensorNaam[DEFAULT_BUFLEN] = "";
+                sscanf(recvbuf, "%[^=]=%s", key, sensorNaam);
+                printf("Ontvangen: %s\n", recvbuf);
+
+                char antwoord[DEFAULT_BUFLEN];
+                snprintf(antwoord, sizeof(antwoord), "%s=%s", sensorNaam, zoekWaarde(sensorNaam));
+                send(ClientSocket, antwoord, (int)strlen(antwoord), 0);
+                printf("Antwoord verstuurd: %s\n", antwoord);
+            }
+        } while (iResult > 0);
+
+        printf("Client verbroken, wacht op nieuwe client...\n\n");
+        closesocket(ClientSocket);
     }
-    printf("Client verbonden!\n");
+
     closesocket(ListenSocket);
-
-    char recvbuf[DEFAULT_BUFLEN];
-    int iResult;
-    do {
-        iResult = recv(ClientSocket, recvbuf, DEFAULT_BUFLEN - 1, 0);
-        if (iResult > 0) {
-            recvbuf[iResult] = '\0';
-            printf("Ontvangen: %s\n", recvbuf);
-            const char* antwoord = "Hallo van de server!";
-            send(ClientSocket, antwoord, (int)strlen(antwoord), 0);
-        }
-    } while (iResult > 0);
-
-    closesocket(ClientSocket);
     WSACleanup();
     return 0;
 }
